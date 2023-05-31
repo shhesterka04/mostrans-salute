bb1010becb58d16d7980c50cc1c7bb89a604a674
Artyom <artemeroshkin47@gmail.com>
2023-06-01 00:35:36 +0300

    Написал парсер

--- src/mosru_api/data_getter.py
+++ src/mosru_api/data_getter.py
@@ -1,40 +1,47 @@
 import requests
+from requests import Response
+from tqdm import tqdm
+
 from src.core.config import API_KEY
 import timeit
 
 
 def get_response(dataset):
     data = []
+    length = 0
     url = f"https://apidata-new.mos.ru/v1/datasets/{dataset}/count"
     params = {
         "api_key": API_KEY,
     }
-    response = requests.get(url=url, params=params)
+    response: Response = requests.get(url=url, params=params)
     if response.status_code == 200:
         length = int(response.json())
+        print(length)
     else:
         print(f"Response text: {response.text}")
-    for skip in range(0, length, 500):
-        top = 500
-        url = f"https://apidata-new.mos.ru/v1/datasets/{dataset}/rows"
-        params = {
-            "api_key": API_KEY,
-            "$top": top,
-            "$skip": skip
-        }
-        response = requests.get(url=url, params=params)
-        if response.status_code == 200:
-            data.append(response.json())
-        else:
-            print('Failed to retrieve data from API')
-            print(f"Response text: {response.text}")
+    for skip in tqdm(range(0, length, 500)):
+        data.extend(get_api_data(dataset=dataset, skip=skip))
     return data
+
+
+def get_api_data(dataset, top=500, skip=0):
+    url = f"https://apidata-new.mos.ru/v1/datasets/{dataset}/rows"
+    params = {
+        "api_key": API_KEY,
+        "$top": top,
+        "$skip": skip
+    }
+    response = requests.get(url=url, params=params)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print('Failed to retrieve data from API')
+        print(f"Response text: {response.text}")
 
 
 def get_id_routes():
     data = get_response(60664)
     for row in data:
-        route_short_name = row["Cells"]["route_short_name"]
         route_short_name = row["Cells"]["route_short_name"]
         route_id = row["Cells"]["route_id"]
         route_long_name = row["Cells"]["route_long_name"]
@@ -84,9 +91,13 @@ def get_cal_routes():
 
 start = timeit.default_timer()
 get_id_routes()
-get_trip_id()
+print('ok')
 get_time_and_stops_id()
+print('ok')
+get_trip_id()
+print('ok')
 get_stops_names()
+print('ok')
 get_cal_routes()
 end = timeit.default_timer()
 
