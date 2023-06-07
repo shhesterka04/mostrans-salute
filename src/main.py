560c0ea4086fce78d3a9899c43db094d13c2570f
Artyom <artemeroshkin47@gmail.com>
2023-06-08 00:32:53 +0300

    Зачем?

--- src/main.py
+++ src/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.routers.schedule import schedule_router
 
 
 def get_application() -> FastAPI:
     application = FastAPI()
+
+    origins = [
+        "http://localhost:3001"
+    ]
+
+    application.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     application.include_router(schedule_router)
     return application
